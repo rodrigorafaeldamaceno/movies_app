5c4d90f96288261cdc5ad4963469f08b5570e747
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2024-05-17 09:41:57 -0300

    adjustemnts in moviepage

--- lib/home/pages/movie_page.dart
+++ lib/home/pages/movie_page.dart
@@ -19,128 +19,125 @@ class MoviePage extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       extendBodyBehindAppBar: true,
-      body: SingleChildScrollView(
-        child: FutureBuilder(
-          future: controller.getMovieDetails(movie.id ?? 0),
-          builder: ((context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final details = snapshot.data;
-            return SizedBox(
-              // width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  CachedNetworkImage(
-                    imageUrl: getImageUrl(details?.backdropPath ?? ''),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Text(
-                                details?.title ?? '',
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
+      body: FutureBuilder(
+        future: controller.getMovieDetails(movie.id ?? 0),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final details = snapshot.data;
+          return SingleChildScrollView(
+            // width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CachedNetworkImage(
+                  imageUrl: getImageUrl(details?.backdropPath ?? ''),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(
+                              details?.title ?? '',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
                               ),
                             ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              DateTime.parse(details?.releaseDate ?? '')
-                                  .year
-                                  .toString(),
-                            ),
-                          ],
-                        ),
-                        Builder(builder: (context) {
-                          String text = '${details?.runtime} | ';
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            DateTime.parse(details?.releaseDate ?? '')
+                                .year
+                                .toString(),
+                          ),
+                        ],
+                      ),
+                      Builder(builder: (context) {
+                        String text = '${details?.runtime} | ';
 
-                          text = text +=
-                              (details?.genres?.map((e) => e.name).join(', ') ??
-                                  '');
-                          return Text(text);
-                        }),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        SizedBox(
-                          height: 200,
-                          child: FutureBuilder(
-                            future: controller.getSimilarMovies(
-                                movieId: movie.id ?? 0),
-                            builder:
-                                (BuildContext context, AsyncSnapshot snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              }
-
-                              return SizedBox(
-                                child: ListView.separated(
-                                  shrinkWrap: true,
-                                  itemCount: snapshot.data?.length ?? 0,
-                                  scrollDirection: Axis.horizontal,
-                                  separatorBuilder:
-                                      (BuildContext context, int index) {
-                                    return const SizedBox(
-                                      width: 10,
-                                    );
-                                  },
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return AspectRatio(
-                                      aspectRatio: 2 / 3,
-                                      child: MovieCard(
-                                        movie: snapshot.data[index],
-                                        onTap: () {
-                                          Navigator.push(context,
-                                              MaterialPageRoute(
-                                                  builder: (context) {
-                                            return MoviePage(
-                                                movie: snapshot.data[index]);
-                                          }));
-                                        },
-                                      ),
-                                    );
-                                  },
-                                ),
+                        text = text +=
+                            (details?.genres?.map((e) => e.name).join(', ') ??
+                                '');
+                        return Text(text);
+                      }),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        height: 200,
+                        child: FutureBuilder(
+                          future: controller.getSimilarMovies(
+                              movieId: movie.id ?? 0),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                child: CircularProgressIndicator(),
                               );
-                            },
-                          ),
+                            }
+
+                            return SizedBox(
+                              child: ListView.separated(
+                                shrinkWrap: true,
+                                itemCount: snapshot.data?.length ?? 0,
+                                scrollDirection: Axis.horizontal,
+                                separatorBuilder:
+                                    (BuildContext context, int index) {
+                                  return const SizedBox(
+                                    width: 10,
+                                  );
+                                },
+                                itemBuilder: (BuildContext context, int index) {
+                                  return AspectRatio(
+                                    aspectRatio: 2 / 3,
+                                    child: MovieCard(
+                                      movie: snapshot.data[index],
+                                      onTap: () {
+                                        Navigator.push(context,
+                                            MaterialPageRoute(
+                                                builder: (context) {
+                                          return MoviePage(
+                                              movie: snapshot.data[index]);
+                                        }));
+                                      },
+                                    ),
+                                  );
+                                },
+                              ),
+                            );
+                          },
                         ),
-                        const SizedBox(
-                          height: 10,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        details?.overview ?? '',
+                        style: const TextStyle(
+                          fontSize: 20,
                         ),
-                        Text(
-                          details?.overview ?? '',
-                          style: const TextStyle(
-                            fontSize: 20,
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            );
-          }),
-        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
