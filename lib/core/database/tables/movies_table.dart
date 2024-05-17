733276e48cf2ceee59e4c680cccc87d1be870e83
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2024-05-17 09:09:53 -0300

    fixed Movies table

--- lib/core/database/tables/movies_table.dart
+++ lib/core/database/tables/movies_table.dart
@@ -4,11 +4,16 @@ import 'package:drift/drift.dart';
 class Movies extends Table {
   IntColumn get id => integer().nullable()();
   TextColumn get title => text().nullable()();
-  TextColumn get backdropPath => text().nullable()();
-  TextColumn get releaseDate => text().nullable()();
-  TextColumn get originalTitle => text().nullable()();
-  TextColumn get posterPath => text().nullable()();
   TextColumn get overview => text().nullable()();
+
+  @JsonKey('backdrop_path')
+  TextColumn get backdropPath => text().nullable()();
+  @JsonKey('release_date')
+  TextColumn get releaseDate => text().nullable()();
+  @JsonKey('original_title')
+  TextColumn get originalTitle => text().nullable()();
+  @JsonKey('poster_path')
+  TextColumn get posterPath => text().nullable()();
 
   @override
   Set<Column> get primaryKey => {id};
